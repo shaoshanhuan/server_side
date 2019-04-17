a0ef93bb66dffe9b3b20aa0fbcbab7a5a4e9e430
shaoshanhuan <shaoshanhuan@163.com>
2019-04-17 12:04:55 +0800

    模拟了/me接口

--- app.js
+++ app.js
@@ -6,4 +6,15 @@ app.get("/api",(req,res)=>{
 	res.json({"a" : 10});
 });
 
+app.get("/mingan",(req,res)=>{
+	res.status(401);
+	res.json({"err":-4});
+});
+
+app.get("/me",(req,res)=>{
+	// res.status(401);
+	// res.json({"err":-4});
+	res.json({"username":"xiaoming","nickname":"xiaoming","avatar":"dsfadf.jpg"})
+});
+
 app.listen(3000);
